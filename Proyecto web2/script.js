ecaad89fa17790667d30fe1d4cf101a67a19540b
IsaacRios25 <160376203+IsaacRios25@users.noreply.github.com>
2024-05-23 20:17:32 -0600

    Update script.js

--- Proyecto web2/script.js
+++ Proyecto web2/script.js
@@ -1,17 +1,17 @@
-function iniciar(){
-    var boton=document.getElementById('grabar');
+function iniciar(){     //funcion con la api storage 
+    var boton=document.getElementById('grabar');   //llama al input grabar para que obtenga los datos del los input 
     boton.addEventListener('click', nuevoitem, false);
     mostrar();
     }
     function nuevoitem(){
-    var clave=document.getElementById('clave').value;
+    var clave=document.getElementById('clave').value;   //obtiene lo que esta en los caracteres de los inputs 
     var valor=document.getElementById('texto').value;
-    localStorage.setItem(clave,valor);
+    localStorage.setItem(clave,valor);          //Funcion localsatorage que accede a los datos almacenados dentro de los input 
     mostrar();
     document.getElementById('clave').value='';
     document.getElementById('texto').value='';
     }
-    function mostrar(){
+    function mostrar(){                 //funcion mostrar que hace que recorar por toda la palabra y se muestre en un parrafo dentro de la pagina
     var cajadatos=document.getElementById('cajadatos');
     cajadatos.innerHTML='';
     for(var f=0;f<localStorage.length;f++){
@@ -21,7 +21,7 @@ function iniciar(){
     }
     }
     window.addEventListener('load', iniciar, false);
-    function elimina()
+    function elimina()//funcion que elimina los datos almacenados dentro del navegador 
     {
     
         if(confirm('Está Seguro?')){
